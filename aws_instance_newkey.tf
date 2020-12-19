683ae6587772efc09e4dcde1545f3983be60c054
Mohan Karki <mohankarki@Mohans-MacBook-Pro.local>
2020-12-19 17:38:42 -0600

    Terraform to create the new EC2 instance with new key pair

---
+++ aws_instance_newkey.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example1" {
+  ami           = "ami-04d29b6f966df1537"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-08193d745a3af6f7e"]
+  key_name = "DevOpsKey"
+  tags = {
+   Name = "Terraform"
+   Environment = "Development"
+  }
+}
+
+resource "aws_key_pair" "deploy" {
+ key_name = "Teraform_key"
+ public_key = "ssh-rsa"
+
+}
