fd9a794e62b6e2930f0ccef25208be57187caa5a
mohankarki54 <37468520+mohankarki54@users.noreply.github.com>
2020-12-19 21:07:34 -0600

    Update aws_instance_newkey.tf
    It will output the key needed to SSH for EC2 instance.

--- aws_instance_newkey.tf
+++ aws_instance_newkey.tf
@@ -12,18 +12,27 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "DevOps"
+  public_key = "${tls_private_key.example.public_key_openssh}"
+}
+
 resource "aws_instance" "example1" {
   ami           = "ami-04d29b6f966df1537"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-08193d745a3af6f7e"]
+  key_name      = "${aws_key_pair.generated_key.key_name}"
   tags = {
    Name = "Terraform"
    Environment = "Development"
   }
 }
 
-resource "aws_key_pair" "deploy" {
- key_name = "Teraform_key"
- public_key = "ssh-rsa"
-
+output "DevOps"{
+ value = aws_key_pair.generated_key.public_key
 }
