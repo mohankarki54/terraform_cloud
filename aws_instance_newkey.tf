6e72496dc23d7abe0e14c50ec550e9d729e2847b
mohankarki54 <37468520+mohankarki54@users.noreply.github.com>
2020-12-19 20:37:35 -0600

    Update aws_instance_newkey.tf

--- aws_instance_newkey.tf
+++ aws_instance_newkey.tf
@@ -16,7 +16,6 @@ resource "aws_instance" "example1" {
   ami           = "ami-04d29b6f966df1537"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-08193d745a3af6f7e"]
-  key_name = "DevOpsKey"
   tags = {
    Name = "Terraform"
    Environment = "Development"
